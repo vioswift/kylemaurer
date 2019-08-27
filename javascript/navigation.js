54582319b22d2deb696b816c9170ad7beeca97c7
Vioswift <kylewww@gmail.com>
2019-08-28 07:41:16 +1000

    Mobile version for the navigation is now working

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,15 +1,13 @@
-//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+var isMobile;
+var hamburgerToggle;
 
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
-}
+function onScreenSizeChange(){
+    isMobile = window.matchMedia && window.matchMedia('(max-device-width: 700px)').matches || screen.width <= 960;
+    hamburgerToggle = document.getElementById("hamburgerToggle");
+
+    if (isMobile) {
+        hamburgerToggle.checked = false;
+    }else {
+        hamburgerToggle.checked = true;
+    }
+};
