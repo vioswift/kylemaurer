1384afd08e3b0d98dccfc5442142e838c3e9ea3f
Vioswift <kylewww@gmail.com>
2019-09-03 08:26:40 +1000

    filling out programming skills, change page title on element view test

---
+++ javascript/page.js
@@ -0,0 +1,6 @@
+var observer = new IntersectionObserver(function(entries) {
+	if(entries[0].isIntersecting === true)
+		console.log('Element is fully visible in screen');
+}, { threshold: [1] });
+
+observer.observe(document.querySelector("#programmingskills"));
