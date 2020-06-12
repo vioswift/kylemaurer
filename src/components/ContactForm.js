33e4dcf05d1b2f7fc017d17884ed4913d06b1057
vioswift <Kylewww@gmail.com>
2020-06-12 18:35:44 +1000

    Added Ref's to form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useRef }  from 'react';
 import '../css/buttons/button.css';
 import '../css/inputs/text-input.css';
 import '../css/inputs/label.css';
@@ -10,56 +10,48 @@ const encode = (data) => {
 }
 
 export default function ContactForm() {
-    const [data, setData] = useState(
-        {
-            name : "", 
-            email : "", 
-            subject : "",
-            message : ""
-        }
-    );
-
-    useEffect(() => {
-        setData(
-            {
-                name : "", 
-                email : "", 
-                subject : "",
-                message : ""
-            }
-        );
-    }, []);
+    const name = useRef();
+    const subject = useRef();
+    const email = useRef();
+    const message = useRef();
 
     const handleSubmit = e => {
+        e.preventDefault()
+
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", data })
+            body: encode({ "form-name": "contact", 
+                name: name.current.value,
+                subject: subject.current.value,
+                email: email.current.value,
+                message: message.current.value
+            })
         })
         .then(() => alert("Success!"))
         .catch(error => alert(error));
     
         e.preventDefault();
     };
-    
+
     return (
         <form name="contact" netlify netlify-honeypot="bot-field" onSubmit={handleSubmit.bind()}>
             <input type="hidden" name="form-name" value="contact"/> 
             <div className="row rauto">
                 <label className="label">Name</label> 
-                <input type="text" name="name" className="text-input" placeholder="e.g. Kyle"/>  
+                <input type="text" name="name" ref={name} className="text-input" placeholder="e.g. Kyle"/>  
             </div>
             <div className="row rauto">
                 <label className="label">Email</label> 
-                <input type="email" name="email" className="text-input" placeholder="example@gmail.com"/>                    
+                <input type="email" name="email" ref={email} className="text-input" placeholder="example@gmail.com"/>                    
             </div>
             <div className="row rauto">
                 <label className="label">Subject</label> 
-                <input type="text" name="subject" className="text-input" placeholder="Great Projects"/>
+                <input type="text" name="subject" ref={subject} className="text-input" placeholder="Great Projects"/>
             </div>
             <div className="row rauto">
                 <label className="label">Message</label> 
-                <textarea name="message" className="text-input" placeholder="Message here..." rows="4" cols="66"></textarea>                    
+                <textarea name="message" className="text-input" ref={message} placeholder="Message here..." rows="4" cols="66"></textarea>                    
             </div>
             <div className="row rauto">
                 <hr/>
