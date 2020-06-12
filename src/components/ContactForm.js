c3c9718734bfc6a2cabba54f9363316db5c367ca
vioswift <Kylewww@gmail.com>
2020-06-12 19:48:34 +1000

    changed from again

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React, { useRef }  from 'react';
+import React, { useState }  from 'react';
 import '../css/buttons/button.css';
 import '../css/inputs/text-input.css';
 import '../css/inputs/label.css';
@@ -10,21 +10,30 @@ const encode = (data) => {
 }
 
 export default function ContactForm() {
-    const name = useRef();
-    const subject = useRef();
-    const email = useRef();
-    const message = useRef();
+    const [formData, setFormData] = useState(
+        {
+            name: '',
+            subject: '',
+            email: '',
+            message: ''
+        }
+    );
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+            [e.target.subject]: e.target.value,
+            [e.target.email]: e.target.value,
+            [e.target.message]: e.target.value
+        });
+    };
 
     const handleSubmit = e => {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", 
-                name: name.current.value,
-                subject: subject.current.value,
-                email: email.current.value,
-                message: message.current.value
-            })
+            body: encode({ "form-name": "contact", formData})
         })
         .then(() => alert("Success!"))
         .catch(error => alert(error));
@@ -34,23 +43,22 @@ export default function ContactForm() {
 
     return (
         <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit.bind()}>
-            <input type="hidden" name="form-name" value="contact" /> 
-            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <div className="row rauto">
                 <label className="label">Name</label> 
-                <input type="text" name="name" ref={name} className="text-input" placeholder="e.g. Kyle"/>  
+                <input type="text" name="name" onChange={handleChange} className="text-input" placeholder="e.g. Kyle"/>  
             </div>
             <div className="row rauto">
                 <label className="label">Email</label> 
-                <input type="email" name="email" ref={email} className="text-input" placeholder="example@gmail.com"/>                    
+                <input type="email" name="email" onChange={handleChange} className="text-input" placeholder="example@gmail.com"/>                    
             </div>
             <div className="row rauto">
                 <label className="label">Subject</label> 
-                <input type="text" name="subject" ref={subject} className="text-input" placeholder="Great Projects"/>
+                <input type="text" name="subject" onChange={handleChange} className="text-input" placeholder="Great Projects"/>
             </div>
             <div className="row rauto">
                 <label className="label">Message</label> 
-                <textarea name="message" className="text-input" ref={message} placeholder="Message here..." rows="4" cols="66"></textarea>                    
+                <textarea name="message" onChange={handleChange} className="text-input" placeholder="Message here..." rows="4" cols="66"></textarea>                    
             </div>
             <div className="row rauto">
                 <hr/>
